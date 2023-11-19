10a42850401150e7952413385106f3d427f40c0a
Aasna-S <142115500+Aasna-S@users.noreply.github.com>
2023-11-18 21:48:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,16 @@ import pandas as pd
 # Path to the Excel file
 file_path = "project data without current (8) (4).xlsx"
 
+sheet_names = ['Electricity Demand', 'Generation Output Targets', 'Generation Capacity Targets',
+               'Emissions Factor', 'Cost', 'Emissions Cap', 'Capital Expenditure Budget',
+               'Dev_E', 'Dev_G', 'Dev_Q', 'Dev_C']
+
 # Function to load data from a specific sheet
 def load_data(sheet_name):
     return pd.read_excel(file_path, sheet_name=sheet_name)
 
 # Streamlit app layout
 st.title('Excel Data Dashboard')
-
-# List all the sheet names
-sheet_names = ['Electricity Demand', 'Generation Output Targets', 'Generation Capacity Targets',
-               'Emissions Factor', 'Cost', 'Emissions Cap', 'Capital Expenditure Budget',
-               'Dev_E', 'Dev_G', 'Dev_Q', 'Dev_C']
 
 # Dropdown to select a sheet
 selected_sheet_name = st.selectbox('Select a sheet', sheet_names)
