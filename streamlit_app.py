eb126e874f928aaa28d533f40d7fa83c22837811
Aasna-S <142115500+Aasna-S@users.noreply.github.com>
2023-11-20 21:28:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,52 +4,83 @@ import pandas as pd
 
 # Set page config
 st.set_page_config(page_title="Canada Net Zero", page_icon="🌍", layout="wide")
+
 # Create a DataFrame from model results
 def create_dataframe_from_model_results():
     data = {
-    'Year': [2025]*8 + [2030]*8 + [2035]*8,
-    'Technology': ['wind', 'solar', 'hydro', 'nuclear', 'natural_gas', 'geothermal', 'oil', 'coal']*3,
-    'Generation': [
-        60429.05, 7022.27, 401733.6, 61714.80, 87486.85, 14835.78, 17267.87, 17555.67,
-        92547.75, 15214.42, 443103.1, 192045.74, 19591.28, 7526.04, 0.0, 0.0,
-        384355.62, 42000.0, 460000.0, 200355.62, 0.0, 0.0, 0.0, 0.0
-    ],
-    'Capacity': [
-        64389.48, 12184.92, 401733.6, 78631.37, 106529.07, 14835.78, 17267.87, 17555.67,
-        193168.44, 36554.76, 443103.1, 192045.74, 106529.07, 8281.2, 0.0, 0.0,
-        384355.62, 60924.6, 460000.0, 200355.62, 106529.07, 8281.2, 0.0, 0.0
-    ],
-    'Emissions': [
-        0.9064357500000001, 0.31249102456246486, 9.641606399999999, 1.689093156527198, 
-         42.868556500000004, 0.5637597214768317, 8.461260013019402, 14.395653940518045, 
-        1.38821625, 0.6770416899999999, 10.6344744, 2.3045488282845183, 
-        9.599729311715484, 0.28598952, 0.0, 0.0, 
-        5.765, 1.869, 11.04, 2.40426744, 0.0, 0.0, 0.0, 0.0
-    ],
-    'Cost': [
-        3441403582.340755, 46757597101.6181, 23323799910.84075, 4052626813.89698, 
-        1193777129.8407576, 46757597101.6181, 46757597101.6181, 46757597101.6181, 
-        8350474995.253318, 3996403672.7533255, 28187146477.753326, 17971513125.981358, 
-        1460321671.845374, 1601898273.5533285, 869840362.7533255, 56348716040.62008, 
-        32312707010.675564, 9876242246.075558, 29604242246.07553, 1245242246.0755577, 
-        69704387961.0, 1245242246.0755577, 69704387961.0, 69704387961.0
-    ]
-}
+        'Year': [2025]*8 + [2030]*8 + [2035]*8,
+        'Technology': ['wind', 'solar', 'hydro', 'nuclear', 'natural_gas', 'geothermal', 'oil', 'coal']*3,
+        'Generation': [
+            60429.05, 7022.27, 401733.6, 61714.80, 87486.85, 14835.78, 17267.87, 17555.67,
+            92547.75, 15214.42, 443103.1, 192045.74, 19591.28, 7526.04, 0.0, 0.0,
+            384355.62, 42000.0, 460000.0, 200355.62, 0.0, 0.0, 0.0, 0.0
+        ],
+        'Capacity': [
+            64389.48, 12184.92, 401733.6, 78631.37, 106529.07, 14835.78, 17267.87, 17555.67,
+            193168.44, 36554.76, 443103.1, 192045.74, 106529.07, 8281.2, 0.0, 0.0,
+            384355.62, 60924.6, 460000.0, 200355.62, 106529.07, 8281.2, 0.0, 0.0
+        ],
+        'Emissions': [
+            0.9064357500000001, 0.31249102456246486, 9.641606399999999, 1.689093156527198, 
+            42.868556500000004, 0.5637597214768317, 8.461260013019402, 14.395653940518045, 
+            1.38821625, 0.6770416899999999, 10.6344744, 2.3045488282845183, 
+            9.599729311715484, 0.28598952, 0.0, 0.0, 
+            5.765, 1.869, 11.04, 2.40426744, 0.0, 0.0, 0.0, 0.0
+        ],
+        'Cost': [
+            3441403582.340755, 46757597101.6181, 23323799910.84075, 4052626813.89698, 
+            1193777129.8407576, 46757597101.6181, 46757597101.6181, 46757597101.6181, 
+            8350474995.253318, 3996403672.7533255, 28187146477.753326, 17971513125.981358, 
+            1460321671.845374, 1601898273.5533285, 869840362.7533255, 56348716040.62008, 
+            32312707010.675564, 9876242246.075558, 29604242246.07553, 1245242246.0755577, 
+            69704387961.0, 1245242246.0755577, 69704387961.0, 69704387961.0
+        ]
+    }
     return pd.DataFrame(data)
 
 df = create_dataframe_from_model_results()
 
+# Define custom CSS styles
+st.markdown(
+    """
+    <style>
+    .title {
+        font-size: 28px;
+        color: #007ACC;
+        padding: 10px;
+    }
+    .header {
+        font-size: 24px;
+        color: #333;
+        padding: 10px;
+    }
+    .subheader {
+        font-size: 20px;
+        color: #555;
+        padding: 5px;
+    }
+    .chart-container {
+        background-color: #fff;
+        border-radius: 10px;
+        padding: 20px;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Dashboard layout
-st.title("Canada Power Generation Insights (Goal Programming Model)")
+st.title("Canada Power Generation Insights (Goal Programming Model)", class_="title")
 
 # Canada-specific narrative
-st.write("This dashboard provides an in-depth analysis of power generation in Canada, focusing on emissions, costs, and technology trends. Explore the data and gain insights into Canada's journey towards a sustainable and net-zero emissions future. The results presented here are based on a goal programming model.")
+st.markdown("This dashboard provides an in-depth analysis of power generation in Canada, focusing on emissions, costs, and technology trends. Explore the data and gain insights into Canada's journey towards a sustainable and net-zero emissions future. The results presented here are based on a goal programming model.")
 
 # Create tabs for different sections
 tab1, tab2, tab3 = st.tabs(["Generation Overview", "Emissions Analysis", "Cost Analysis"])
 
 with tab1:
-    st.header("Power Generation Overview")
+    st.header("Power Generation Overview", class_="header")
     st.markdown("Explore power generation data and its impact on emissions reduction.")
     year_options = ['All'] + sorted(df['Year'].unique().tolist())
     year_filter = st.selectbox("Select the Year", options=year_options, key="gen_year_select")
@@ -67,23 +98,23 @@ with tab1:
     kpi1.metric("Total Emissions", f"{total_emission:.2f}", delta=None, delta_color="inverse")
     kpi2.metric("Total Cost", f"${total_cost:,.2f}", delta=None, delta_color="inverse")
 
-    chart_container, table_container = st.columns([2,1])
+    chart_container, table_container = st.columns([2, 1])
     with chart_container:
-        st.markdown("### Generation by Source")
+        st.markdown("### Generation by Source", class_="subheader")
         fig1 = px.bar(filtered_df, x='Year', y='Generation', color='Technology', barmode='group')
-        st.plotly_chart(fig1, height=300, width = 400)  # Adjust height as needed
+        st.plotly_chart(fig1, height=400)  # Adjust height as needed
     with table_container:
-        st.subheader("Data Table for Generation Overview")
-        st.dataframe(filtered_df, height=300)  # Adjust height as needed
+        st.subheader("Data Table for Generation Overview", class_="subheader")
+        st.dataframe(filtered_df, height=400)  # Adjust height as needed
 
-    st.subheader("Interactive Line Chart for Energy Generation")
+    st.subheader("Interactive Line Chart for Energy Generation", class_="subheader")
     selected_tech = st.selectbox("Select Technology", df['Technology'].unique(), key="gen_tech_select")
     filtered_data = df[df['Technology'] == selected_tech]
     fig_line = px.line(filtered_data, x="Year", y="Generation", title=f"Generation Over Time for {selected_tech}")
     st.plotly_chart(fig_line)
 
 with tab2:
-    st.header("Emissions Overview")
+    st.header("Emissions Overview", class_="header")
     st.markdown("Explore greenhouse gas emissions data in relation to power generation.")
     year_options = ['All'] + sorted(df['Year'].unique().tolist())
     year_filter = st.selectbox("Select the Year", options=year_options, key="emissions_year_select")
@@ -92,16 +123,16 @@ with tab2:
     else:
         filtered_df = df[df['Year'] == year_filter]
     
-    st.markdown("### Emissions by Source")
+    st.markdown("### Emissions by Source", class_="subheader")
     fig2 = px.bar(filtered_df, x='Year', y='Emissions', color='Technology', barmode='group')
     st.plotly_chart(fig2)
     
-    st.subheader("Emissions Over Time")
+    st.subheader("Emissions Over Time", class_="subheader")
     fig_line_emissions = px.line(df, x='Year', y='Emissions', color='Technology', title="Emissions Over Time")
     st.plotly_chart(fig_line_emissions)
 
 with tab3:
-    st.header("Cost Analysis")
+    st.header("Cost Analysis", class_="header")
     st.markdown("Analyze the cost breakdown of power generation technologies.")
     year_options = ['All'] + sorted(df['Year'].unique().tolist())
     year_filter = st.selectbox("Select the Year", options=year_options, key="cost_year_select")
@@ -113,7 +144,7 @@ with tab3:
     fig3 = px.bar(filtered_df, x='Year', y='Cost', color='Technology', barmode='group')
     st.plotly_chart(fig3)
 
-    st.subheader("Bar Chart for Cost Breakdown")
+    st.subheader("Bar Chart for Cost Breakdown", class_="subheader")
     selected_year = st.select_slider("Select Year", options=df['Year'].unique(), value=df['Year'].max(), key="cost_slider")
     cost_breakdown_data = df[df['Year'] == selected_year]
     fig_cost_breakdown = px.bar(cost_breakdown_data, x='Technology', y='Cost', title=f"Cost Breakdown for {selected_year}")
